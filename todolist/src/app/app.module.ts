69e07d8e61084382a685c6a7943cc17cf0badf94
Softwerick <erickhescola@gmail.com>
2021-07-10 16:16:30 -0300

    Adicionar header e footer

--- todolist/src/app/app.module.ts
+++ todolist/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule }   from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
